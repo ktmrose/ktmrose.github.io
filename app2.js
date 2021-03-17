97b46db39ddf35ef28ddd447a590fc4950d9e90f
ktmrose <ktm.rose@yahoo.com>
2021-03-16 18:59:58 -0600

    api calls

--- app2.js
+++ app2.js
@@ -1,6 +1,8 @@
 var redirectUri = 'https://ktmrose.github.io';
 var clientId = "";
 var clientSec = "";
+var access_token = null;
+var refresh_token = null;
 
 const scopes = [
     'user-read-playback-state',
@@ -14,6 +16,7 @@ const scopes = [
 ];
 
 const AUTHORIZE = "https://accounts.spotify.com/authorize";
+const TOKEN = "https://accounts.spotify.com/api/token";
 
 function getcode() {
     let code = null;
@@ -22,17 +25,65 @@ function getcode() {
         const urlParams = new URLSearchParams(queryString);
         code = urlParams.get('code');
     }
-    console.log(code);
+    // console.log(code);
     return code;
+}
+
+function handleAuthorizationResponse() {
+    if (this.status === 200) {
+
+        var data = JSON.parse(this.responseText);
+        if (data.access_token !== undefined) {
+            access_token = data.access_token;
+            localStorage.setItem("access_token", access_token);
+        }
+        if (data.refresh_token !== undefined) {
+            refresh_token = data.refresh_token;
+            localStorage.setItem("refresh_token", refresh_token);
+        }
+        onPageLoad();
+    } else {
+        console.log(this.responseText);
+        alert(this.responseText);
+    }
+}
+
+function callAuthorizationApi(body) {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", TOKEN, true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.setRequestHeader('Authorization', 'Basic ' + btoa(clientId + ":" + clientSec));
+    xhr.send(body);
+    xhr.onload = handleAuthorizationResponse;
+}
+
+function fetchAccessToken(code) {
+    let body = "grant_type=authorization_code";
+    body += "&code=" + code;
+    body += "&redirect_uri=" + encodeURI(redirectUri);
+    body += "&client_id=" + clientId;
+    body += "&client_secret=" + clientSec;
+    callAuthorizationApi(body);
 }
 
 function handleRedirect() {
     let code = getcode();
+    fetchAccessToken(code);
+    window.history.pushState("", "", redirectUri);
 }
 
 function onPageLoad() {
+    clientId = localStorage.getItem("client_id");
+    clientSec = localStorage.getItem("client_secret");
     if (window.location.search.length > 0) {
         handleRedirect();
+    } else {
+        access_token = localStorage.getItem("access_token");
+        if (access_token == null) {
+            document.getElementById("tokenSection").style.display = 'block';
+        } else {
+
+        }
     }
 }
 
